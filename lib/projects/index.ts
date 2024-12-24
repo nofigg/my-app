47763866ff393a6ecccd3fd226c83e863e16e258
Nolan Figueroa <nfigueroa06@gmail.com>
2024-12-23 19:59:56 -0700

    fix: update Project type import in projects/index.ts

--- lib/projects/index.ts
+++ lib/projects/index.ts
@@ -1,9 +1,9 @@
-import { Project as ProjectTemplate } from './project-template'
+import { Project } from '../types'
 import { dynamicMazeGame } from './dynamic-maze-game'
 import { hardLookAtV0Prompting } from './hard-look-at-v0-prompting'
 import { cadServicesLogo } from './cad-services-logo'
 
-export type Project = ProjectTemplate
+export type Project = Project
 
 export const projects = [
     dynamicMazeGame,
