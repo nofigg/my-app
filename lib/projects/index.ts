14e4a790f017113abaa7c1c2e300cfd86cbdecd0
Nolan Figueroa <nfigueroa06@gmail.com>
2024-12-23 20:05:18 -0700

    fix: update project imports to use correct export names with aliases

--- lib/projects/index.ts
+++ lib/projects/index.ts
@@ -1,7 +1,7 @@
 import type { Project as ProjectType } from '../types'
-import { dynamicMazeGame } from './dynamic-maze-game'
-import { hardLookAtV0Prompting } from './hard-look-at-v0-prompting'
-import { cadServicesLogo } from './cad-services-logo'
+import { project as dynamicMazeGame } from './dynamic-maze-game'
+import { project as hardLookAtV0Prompting } from './hard-look-at-v0-prompting'
+import { project as cadServicesLogo } from './cad-services-logo'
 
 export type Project = ProjectType
 
